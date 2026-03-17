d5af3101f1a0de518145347e49a240c1780bbe3e
yaakovLowenstein <lowensteinyaakov@gmail.com>
2026-03-16 20:34:58 -0400

    Update

--- DNSServer.py
+++ DNSServer.py
@@ -90,7 +90,7 @@ dns_records = {
     'nyu.edu.': {
         dns.rdatatype.A: '192.168.1.106',
         dns.rdatatype.TXT:str(encrypted_value), # Store the encrypted value as a TXT record
-        dns.rdatatype.MX: (10, 'mxa-00256a01.gslb.pphosted.com.'),
+        dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],
         dns.rdatatype.AAAA:' 2001:0db8:85a3:0000:0000:8a2e:0373:7312',
         dns.rdatatype.NS: 'ns1.nyu.edu.',
     }
